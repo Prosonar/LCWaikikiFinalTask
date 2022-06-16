92fd0ce8e3fd8b1981737bd5cd1bf8780f60371b
Sukru Sonar <sukrusonar245@gmail.com>
2022-06-16 22:17:48 +0300

    web api oluşturuldu.

---
+++ WebApi/Controllers/BrandController.cs
@@ -0,0 +1,65 @@
+﻿using Business.Abstract.Services;
+using Entity.Concrete;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BrandController : Controller
+    {
+        IBrandService _brandService;
+
+        public BrandController(IBrandService brandService)
+        {
+            _brandService = brandService;
+        }
+
+        [HttpGet("getall")]
+        [Authorize(Roles = "Admin")]
+        public IActionResult GetAll()
+        {
+            var result = _brandService.GetBrands();
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result.Message);
+        }
+        [HttpPost]
+        public IActionResult Add(Brand color)
+        {
+            var result = _brandService.Add(color);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result.Message);
+        }
+        [HttpDelete]
+        public IActionResult Delete(Brand color)
+        {
+            var result = _brandService.Delete(color);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result.Message);
+        }
+        [HttpPut]
+        public IActionResult Update(Brand color)
+        {
+            var result = _brandService.Update(color);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result.Message);
+        }
+    }
+}
---
+++ WebApi/Controllers/ColorController.cs
@@ -0,0 +1,62 @@
+﻿using Business.Abstract.Services;
+using Entity.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]//route ayarları değişti.Meraktan yaptım.Action denen yere fonksiyonun ismi yazılırsa çalışır.
+    [ApiController]
+    public class ColorController : ControllerBase
+    {
+        IColorService _colorService;
+        public ColorController(IColorService colorService)
+        {
+            _colorService = colorService;
+        }
+        [HttpGet("getall")]
+        //[Authorize()]
+        public IActionResult GetAll()
+        {
+            var result = _colorService.GetColors();
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result.Message);
+        }
+        [HttpPost("add")]
+        public IActionResult Add(Color color)
+        {
+            var result = _colorService.Add(color);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result.Message);
+        }
+        [HttpDelete]
+        public IActionResult Delete(Color color)
+        {
+            var result = _colorService.Delete(color);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result.Message);
+        }
+        [HttpPut]
+        public IActionResult Update(Color color)
+        {
+            var result = _colorService.Update(color);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result.Message);
+        }
+    }
+}
