8dac9f6eff2e1d12b1558bcf3c3b1219b99ebe89
Sukru Sonar <sukrusonar245@gmail.com>
2022-06-16 18:59:48 +0300

    düzenlemeler.

---
+++ Core/CrossCuttingConcerns/Validation/ValidationTool.cs
@@ -0,0 +1,29 @@
+﻿//using Core.Entity;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.CrossCuttingConcerns.Validation
+{
+    public static class ValidationTool
+    {
+        //public static void Validate(IValidator validator,object entity)
+        //{
+        //    var result = validator.Validate((IValidationContext)entity);
+        //    if(result.Errors.Count > 0)
+        //    {
+        //        throw new ValidationException(result.Errors);
+        //    }
+        //}
+        public static void Validate(IValidator validator, object entity)
+        {
+            var context = new ValidationContext<object>(entity);
+            var result = validator.Validate(context);
+            if (!result.IsValid)
+            {
+                throw new ValidationException(result.Errors);
+            }
+        }
+    }
+}
---
+++ Core/DataAccess/BaseRepositories/IEntityRepository.cs
@@ -0,0 +1,18 @@
+﻿using Core.Entity;
+using Core.Entity.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Core.DataAccess.BaseRepositories
+{
+    public interface IEntityRepository<T> where T: class,IEntity,new()
+    {
+        List<T> GetAll(Expression<Func<T, bool>> filter = null);
+        T GetById(Expression<Func<T, bool>> filter);
+        void Add(T entity);
+        void Delete(T entity);
+        void Update(T entity);
+    }
+}
